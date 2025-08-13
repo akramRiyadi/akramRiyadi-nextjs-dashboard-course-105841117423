f625fe563f86391bcc39bc5456cb02d0ee6ff38e
akramRiyadi <157444330+akramRiyadi@users.noreply.github.com>
2025-08-14 00:12:38 +0800

    Upload project Next.js

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import './ui/global.css';
+import { Inter } from 'next/font/google';
+import { Metadata } from 'next';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Acme Dashboard',
+  description: 'Example dashboard built with Next.js',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
